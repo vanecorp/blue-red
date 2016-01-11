7b1e72a1c46f88ec3d4f483c35d5bfa6810aebfc
Vanessa Corpus <vanessa@vanessas-macbook-pro.local>
2016-01-10 18:58:32 -0600

    Initial Commit

---
+++ Blue RedTests/Blue_RedTests.swift
@@ -0,0 +1,36 @@
+//
+//  Blue_RedTests.swift
+//  Blue RedTests
+//
+//  Created by Vanessa on 1/10/16.
+//  Copyright © 2016 Vanessa. All rights reserved.
+//
+
+import XCTest
+@testable import Blue_Red
+
+class Blue_RedTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
